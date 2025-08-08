ef793c88255da04554f607d1cd562799e89f82ba
Fabio Trevizolli <fabiorabello08@gmail.com>
2025-08-08 16:19:09 -0300

    Começo história

---
+++ ptfinal.js
@@ -0,0 +1,39 @@
+//Atributos do personagem:
+const nome = "Arthur Targaryen";
+let classe = "Rei Mago"; //Maior classe possivel
+let racaPersonagem = "Humano";
+let nivel = 10000000000000000000000000000000000;
+let vida = 100000000000000000000;
+let inteligência = 1000000;
+let moedas = 10000000000;
+let xp = 100000000000000000;
+
+//Novos atributos:
+let manaMaxima = 50000000000;
+let forcaBruta = 150000000000000000;
+let defesa = 124000000;
+let agilidade = 12000000;
+
+//Atributos da Arma:
+const NOMEARMA = "Cumpridor de Promessas"; //Cajado
+let DANO_BASE = 100000000000000000000000000000000000;
+const RARIDADEARMA = "Divino";
+const ELEMENTOARMA = "Amaterasu"; //Chama negra que nunca apaga
+
+//Atributos da Armadura:
+const NOMEARMADURA = "Drogon";
+let DEFESA_BASE = 1000000000000000000000000000000;
+const RARIDADEARMADURA = "Divino";
+const MATERIAL = "Aço Valiriano";
+
+
+//=====Novos atributos para aventura=====//
+let inventario = ["Cumpridor de Promessas(Cajado)", "Adaga encantada", "Drogon(Armadura"];
+let aliados = ["Zagreu", "Kasume", "Piter", "Frodo"];
+let inimigosEncontrados = ["Demônios", "Minotauro", "Goblins"];
+let tesourosEncontrados = [];
+
+console.log(`${nome} em seus dias tranquilos na seu novo castelo se lembra de uma vez que foi chamado para limpar o temido... O Castelo Infinito`);
+console.log("==================================");
+console.log("🏰Lembranças Castelo Infinito🏰")
+console.log("==================================");
